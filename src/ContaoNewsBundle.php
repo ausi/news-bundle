f96af68dce895c1cfc1e28ab46362459a0383277
Leo Feyer <github@contao.org>
2014-10-01 21:23:41 +0200

    Move the resources into the `Resources/` folder

---
+++ src/ContaoNewsBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+namespace Contao\Bundle\NewsBundle;
+
+use Contao\Bundle\CoreBundle\HttpKernel\Bundle\ContaoBundle;
+
+class ContaoNewsBundle extends ContaoBundle
+{
+}
