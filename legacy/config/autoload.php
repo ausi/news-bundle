78791092f863b3dd7f956f9beb75f791b333d96a
Leo Feyer <github@contao.org>
2014-10-27 10:39:43 +0100

    Move the legacy resources into the `legacy` folder

---
+++ legacy/config/autoload.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package News
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+// Templates
+TemplateLoader::addFiles(
+[
+	'mod_newsarchive'   => 'vendor/contao/module-news/legacy/templates/modules',
+	'mod_newslist'      => 'vendor/contao/module-news/legacy/templates/modules',
+	'mod_newsmenu'      => 'vendor/contao/module-news/legacy/templates/modules',
+	'mod_newsmenu_day'  => 'vendor/contao/module-news/legacy/templates/modules',
+	'mod_newsmenu_year' => 'vendor/contao/module-news/legacy/templates/modules',
+	'mod_newsreader'    => 'vendor/contao/module-news/legacy/templates/modules',
+	'news_full'         => 'vendor/contao/module-news/legacy/templates/news',
+	'news_latest'       => 'vendor/contao/module-news/legacy/templates/news',
+	'news_short'        => 'vendor/contao/module-news/legacy/templates/news',
+	'news_simple'       => 'vendor/contao/module-news/legacy/templates/news',
+]);
