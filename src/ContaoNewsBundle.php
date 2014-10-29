69ac50e2063f03387322a776b03378038c171d71
Leo Feyer <github@contao.org>
2014-10-29 16:40:04 +0100

    Replace the redundant Contao\Bundle namespace

--- src/ContaoNewsBundle.php
+++ src/ContaoNewsBundle.php
@@ -9,9 +9,9 @@
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
 
-namespace Contao\Bundle\NewsBundle;
+namespace Contao\NewsBundle;
 
-use Contao\Bundle\CoreBundle\HttpKernel\Bundle\ContaoBundle;
+use Contao\CoreBundle\HttpKernel\Bundle\ContaoBundle;
 
 class ContaoNewsBundle extends ContaoBundle
 {
